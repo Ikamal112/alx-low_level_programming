fe5954b9018a5f581e6f38b45cdb5b71e688d6e0
Ikamal112 <kamaludeeniddrisu@gmail.com>
2022-12-31 06:08:36 -0800

    lcl

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lcl;
+
+	for (lcl = 'a'; lcl <= 'z'; lcl++)
+	{
+		putchar(lcl);
+	}
+	puchar('\n');
+	return (0);
+}
