d3f6ab27fc9fbf75a626c10a206ba86ed4168ce9
Ikamal112 <kamaludeeniddrisu@gmail.com>
2023-01-02 06:00:03 -0800

    8hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * jack-bauer - prints every minute of the day
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0.
  */
 
 void jack_bauer(void)
@@ -13,14 +15,13 @@ void jack_bauer(void)
 	for (a = 0; a <= 23; a++)
 	{
 		for (b = 0; b <= 59; b++)
-
 		{
-			_putchar((a / 10) + '0');
-			_putchar((a % 10) + '0');
-			_putchar(':');
-			_putchar((b / 10) + '0');
-			_putchar((b % 10) + '0');
-			_putchar('\n');
+			_putchar (a / 10 + '0');
+			_putchar (a % 10 + '0');
+			_putchar (':');
+			_putchar (b / 10 + '0');
+			_putchar (b % 10 + '0');
+			_putchar ('\n');
 		}
 	}
 }
