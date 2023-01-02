5d6e734bd94fe0907c7b53ee1a106c2489f95dfe
Ikamal112 <kamaludeeniddrisu@gmail.com>
2023-01-02 05:52:19 -0800

    100t

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ void print_times_table(int n)
 	int product;
 
 	if (n < 0 || n >= 15)
-		return;
+		return (0);
 	for (row = 0; row <= n; row++)
 	{
 		for (column = 0; column <= n; column++)
@@ -48,4 +48,5 @@ void print_times_table(int n)
 		}
 		_putchar('\n');
 	}
+	
 }
